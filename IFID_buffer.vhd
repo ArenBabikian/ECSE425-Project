5090297d01a9f27f17b7980db18fe3242b788916
fbinette01 <f.binette01@gmail.com>
2018-03-30 08:58:56 -0400

    Small Changes
    - Removed Register1 and Register2
    - Changed DOWNTO from TO

--- IFID_buffer.vhd
+++ IFID_buffer.vhd
@@ -7,10 +7,8 @@ PORT( clock : IN STD_LOGIC;
       pc_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
       pc_en : OUT STD_LOGIC;
       stall_request : IN STD_LOGIC;
-      IR : IN STD_LOGIC_VECTOR(0 TO 31);
-      IR_out : OUT STD_LOGIC_VECTOR(0 TO 31);
-      IFID_REGISTER1 : OUT STD_LOGIC_VECTOR(4 DOWNTO 0);
-      IFID_REGISTER2 : OUT STD_LOGIC_VECTOR(4 DOWNTO 0));
+      IR : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+      IR_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
 END ifid_buffer;
 
 ARCHITECTURE ifid_buffer_arch OF ifid_buffer IS
@@ -20,8 +18,6 @@ tmpIR <= "00000000000000000000000000100000" WHEN (stall_request = '1') ELSE IR;
 buffer : PROCESS(clock)
 BEGIN
   IF (clock'EVENT AND clock = '1')
-    IFID_REGISTER1 <= tmpIR(6 TO 10);
-    IFID_REGISTER2 <= tmpIR(11 TO 15);
     pc_out <= pc_in;
     pc_en <= '0' WHEN (stall_request = '1') ELSE '1';
     IR_out <= tmpIR;
