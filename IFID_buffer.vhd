7f56ffd298b27602ee49c05fa9ac8ff678856b38
fbinette01 <f.binette01@gmail.com>
2018-03-30 10:50:27 -0400

    Removed pc_en and stall_request
    pc_en and stall requesting will be dealt with directly in IF, no need to have it run in the buffer first.

--- IFID_buffer.vhd
+++ IFID_buffer.vhd
@@ -4,10 +4,8 @@ USE IEEE.STD_LOGIC_1164.ALL;
 ENTITY ifid_buffer IS
 PORT( clock : IN STD_LOGIC;
       pc_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-      stall_request : IN STD_LOGIC;
       IR : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
       pc_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-      pc_en : OUT STD_LOGIC;
       IR_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
 END ifid_buffer;
 
@@ -20,11 +18,6 @@ BEGIN
   IF (clock'EVENT AND clock = '1') THEN
     pc_out <= pc_in;
     IR_out <= tmpIR;
-    IF (stall_request = '1') THEN
-      pc_en <= '0';
-    ELSE
-      pc_en <= '1';
-    END IF;
   END IF;
 END PROCESS;
 END ifid_buffer_arch;
