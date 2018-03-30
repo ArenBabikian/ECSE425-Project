5b55bcb5136af36b056e5a0633137ed5735c8b0a
fbinette01 <f.binette01@gmail.com>
2018-03-30 11:41:08 -0400

    Bug Fixes

--- hazard_detection.vhd
+++ hazard_detection.vhd
@@ -3,14 +3,14 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY hazard_detection is
-  PORT( IDEX_REGISTER : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
-        IFID_rs : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
-        IFID_rt : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+  PORT( rd : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+        rs : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+        rt : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
         STALL_REQUEST : OUT STD_LOGIC);
 END hazard_detection;
 
 ARCHITECTURE hazard_detection_arch of hazard_detection is
 BEGIN
-  STALL_REQUEST <= '1' WHEN (IDEX_REGISTER = IFID_rs or IDEX_REGISTER = IFID_rd)
+  STALL_REQUEST <= '1' WHEN (rd = rs or rd = rt)
                        ELSE '0';
 END hazard_detection_arch;
