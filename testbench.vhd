05939cae57fd62410aebd7ca908020b983680502
ArenBabikian <aren.babikian@mail.mcgill.ca>
2018-03-30 17:10:21 -0400

    finalizing testbench

--- testbench.vhd
+++ testbench.vhd
@@ -10,21 +10,28 @@ END testbench;
 ARCHITECTURE behaviour OF testbench IS
 
 COMPONENT pipeline IS
-port (clk : in std_logic;
-      a, b, c, d, e : in integer;
-      op1, op2, op3, op4, op5, final_output : out integer
+port (
+	clk : in std_logic;
+	reset : in std_logic;
+	initializeMem : in std_logic;
+	writeInstrData : in std_logic_vector(31 DOWNTO 0);
+	memoryread : in std_logic;
+	address_data : in std_logic_vector(31 DOWNTO 0);
+	data : out std_logic_vector(31 DOWNTO 0);
+	registerRead : in std_logic;
+	register_address : in std_logic_vector(4 DOWNTO 0);
+	registerData : out std_logic_vector(31 DOWNTO 0)
   );
 END COMPONENT;
 
 --The input signals with their initial values
-SIGNAL clk: STD_LOGIC := '0';
-SIGNAL s_a, s_b, s_c, s_d, s_e : INTEGER := 0;
-SIGNAL s_op1, s_op2, s_op3, s_op4, s_op5, s_final_output : INTEGER := 0;
-
+SIGNAL clk, reset, initMem, memread, regread: STD_LOGIC := '0';
+SIGNAL writeInstrData, addr_data, data, regdata : std_logic_vector(31 downto 0) := (others => '0') ;
+SIGNAL reg_addr : std_logic_vector(4 downto 0);
 --components needed for reading from and writing to file
 FILE file_input : text;
 FILE file_output_reg : text;
-FILE file_output_mem : text;
+FILE file_output_ram : text;
 SIGNAL input_command : STD_LOGIC_VECTOR (31 downto 0);
 SIGNAL reg_content : STD_LOGIC_VECTOR (31 downto 0);
 SIGNAL ram_content : STD_LOGIC_VECTOR (31 downto 0);
@@ -33,7 +40,7 @@ CONSTANT clk_period : time := 1 ns;
 
 BEGIN
 dut: pipeline
-PORT MAP(clk, s_a, s_b, s_c, s_d, s_e, s_op1, s_op2, s_op3, s_op4, s_op5, s_final_output);
+PORT MAP(clk, reset, initMem, writeInstrData, memread, addr_data, data, regread, reg_addr, regdata);
 
  --clock process
 clk_process : PROCESS
@@ -51,28 +58,31 @@ test_process: PROCESS
 	VARIABLE reg_line : line;
 	VARIABLE ram_line : line;
     VARIABLE input_cmd : std_logic_vector(31 downto 0);
-    VARIABLE reg_cmd : std_logic_vector(31 downto 0);
-    VARIABLE mem_cmd : std_logic_vector(31 downto 0);	
+    VARIABLE reg_content_var : std_logic_vector(31 downto 0);
+    VARIABLE ram_content_var : std_logic_vector(31 downto 0);	
 	variable ind : integer := 0;
 
 	BEGIN
 	--read inputs from file line by line and store the content into the instruction memory
+	--setting initmem to 1 allows to write to the intruction memory
+	initMem <= '1';
 	file_open(file_input, "program.txt", read_mode);
     while not endfile(file_input) loop
         readline(file_input, input_line);
         read(input_line, input_cmd);
 		
+		writeInstrData <= input_cmd;
 		
-		
-		
-		
-        <= input_cmd;
-xxxxx
+		wait for 1 * clk_period;
+		--at every clock cycle, the PC is incremented by 4, thus every time we
+		--write something to memory, we writ it in the following position
+
     end loop;
     file_close(file_input);
 	
-	
-	
+	initmem <= '0';
+	writeInstrData <= "00000000000000000000000000000000";
+	reset <= '1';
 	
 	wait for 10000 * clk_period;
 		
@@ -80,42 +90,44 @@ xxxxx
 	
 	--go through the registers array and print its content to a file
 	--note that there are 32 registers
+	regread <= '1';
 	file_open(file_output_reg, "register_file.txt", write_mode);
 	while ind < 32 loop	
-xxxxx	
-		
-		<= std_logic_vector(to_unsigned(ind, 'length));
-		
+
+		reg_addr <= std_logic_vector(to_unsigned(ind, reg_addr'length));
 		WAIT FOR  1 * clk_period;
 		
-
-		reg_content_var := 
+		reg_content_var := regdata; 
 		write(reg_line, reg_content_var);
 		writeline(file_output_reg, reg_line);		
 		ind := ind +1;
 	end loop;
 	file_close(file_output_reg);
+	
+	regread <= '0';
+	reg_addr <= "00000";
 
 	
 	--go through the data memory array and print its content to a file
 	--note that there are 32768
 	ind := 0;
-	file_open(file_output_mem, "memory.txt", write_mode);
+	memread <= '1';
+	file_open(file_output_ram, "memory.txt", write_mode);
 	while ind < 32768 loop	
-xxxxx	
-
-		<= std_logic_vector(to_unsigned(ind, 'length));
-		
-		
+	
+		addr_data <= std_logic_vector(to_unsigned(ind, addr_data'length));
 		WAIT FOR  1 * clk_period;
 		
 		
-		ram_content_var := 
+		ram_content_var := data;
 		write(ram_line, ram_content_var);
-		writeline(file_output, ram_line);		
+		writeline(file_output_ram, ram_line);		
 		ind := ind +4;
 	end loop;
-	file_close(file_output_mem);
+	file_close(file_output_ram);
+	
+	memread <= '0';
+	data <= "00000000000000000000000000000000";
 	
 	
 	WAIT;
